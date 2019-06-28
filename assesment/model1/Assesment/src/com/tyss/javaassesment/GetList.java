e760ed6a40d73b88c67b291088401b287bbfdd77
Bhavani <bhavanigmgowda@gmail.com>
2019-06-28 17:09:59 +0530

    assesment

--- assesment/model1/Assesment/src/com/tyss/javaassesment/GetList.java
+++ assesment/model1/Assesment/src/com/tyss/javaassesment/GetList.java
@@ -1,7 +1,15 @@
 package com.tyss.javaassesment;
 
 public class GetList {
-	void get(ListElement l) {
-		l.getList();
+	ListElement get(int i) {
+	if (i==1) {
+		return new ArrayListOne();	
 	}
+	else if(i==2) {
+		return new LinkedListTwo();
+	}
+	else {
+		return new VectorThree();
+	}
+}
 }
---
+++ assesment/model1/Assesment/src/com/tyss/javaassesment/ListTest.java
@@ -0,0 +1,15 @@
+package com.tyss.javaassesment;
+/**
+ *WAP for a method which returns ArrayList, LinkedList, Vector… based on the option entered 
+ *1.ArrayList
+ *2.LinkedList
+ *3.Vector
+ */
+public class TestList {
+	public static void main(String[] args) {
+		
+		GetList g = new GetList();
+		g.get(3);
+	}
+
+}
