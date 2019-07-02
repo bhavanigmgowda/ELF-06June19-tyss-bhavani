d864d130ecc411f2e7973f9a9f4a4165efd0c7e7
Bhavani <bhavanigmgowda@gmail.com>
2019-07-02 10:33:27 +0530

    html, css  and JDBC

---
+++ jdbcapp/src/main/java/com/tyss/jdbcapp/TryRes.java
@@ -0,0 +1,56 @@
+package com.tyss.jdbcapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Driver;
+
+import lombok.extern.java.Log;
+@Log
+public class TryRes {
+	public static void main(String[] args) {
+		Driver driver = null;
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+	
+		try(
+				driver = new Driver();
+
+				DriverManager.deregisterDriver(driver);
+
+				String dbUrl = "jdbc:mysql://localhost:3306/tyss_db?user=root&password=root";
+				con = DriverManager.getConnection(dbUrl);
+
+				String query = "select * from EMPLOYEE_INFO";
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(query);
+				){
+			while (rs.next()) {
+				log.info("  ID (PK)	===>	" + rs.getInt("ID"));
+				log.info("  NAME	===>	" + rs.getString("NAME"));
+				log.info("  AGE		===>		" + rs.getInt("AGE"));
+				log.info("  GENDER	===>	" + rs.getString("GENDER"));
+				log.info("  SALARY	===>	" + rs.getInt("SALARY"));
+				log.info("  PHNUM	===>	" + rs.getInt("PHNUM"));
+				log.info("  JOINING_DATE ===>" + rs.getDate("JOINING_DATE"));
+				log.info("  ACCOUNT_NUMBER  ===>" + rs.getInt("ACCOUNT_NUMBER"));
+				log.info("  EMAIL		===>" + rs.getString("EMAIL"));
+				log.info("	DESIGNATION	===>" + rs.getString("DESIGNATION"));
+				log.info("  DOB		===>	" + rs.getDate("DOB"));
+				log.info("  DEPT_ID(FK) ===>" + rs.getInt("DEPT_ID"));
+				log.info("  MNGR_ID ===>	" + rs.getInt("MNGR_ID"));
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
